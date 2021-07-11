b39acd4746f72d437118d10da73ce2c960350cf7
Shikhar8993 <82313777+Shikhar8993@users.noreply.github.com>
2021-07-11 18:22:28 +0530

    Update attacker.js

--- attacker.js
+++ attacker.js
@@ -8,7 +8,7 @@ class Attacker{
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
-      this.image=loadImage("images/Superhero-01.png");
+      this.image=loadImage("Superhero-01.png");
     
       World.add(world, this.body);
     }
